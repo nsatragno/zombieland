0d39c092ad4570c804b1007b67830402a7a30bae
Ivaan <ivaanmr@gmail.com>
2015-05-31 20:27:32 -0300

    Agregado Suites de Test faltantes

--- zombieland-comunes/src/com/rzg/zombieland/test/comunes/TodosLosTests.java
+++ zombieland-comunes/src/com/rzg/zombieland/test/comunes/TodosLosTests.java
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.rzg.zombieland.comunes.test.comunicacion.PeticionRegistroTest;
+
 @RunWith(Suite.class)
-@SuiteClasses({})
+@SuiteClasses({PeticionRegistroTest.class})
 public class TodosLosTests {
 
 }
--- zombieland-servidor/src/com/rzg/zombieland/test/server/TodosLosTests.java
+++ zombieland-servidor/src/com/rzg/zombieland/test/server/TodosLosTests.java
@@ -4,11 +4,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.rzg.zombieland.test.server.comunicacion.SuiteComunicacion;
 import com.rzg.zombieland.test.server.meta.SuiteMeta;
 import com.rzg.zombieland.test.server.persistencia.SuitePersistencia;
 
 @RunWith(Suite.class)
-@SuiteClasses({SuitePersistencia.class, SuiteMeta.class})
+@SuiteClasses({SuitePersistencia.class, SuiteMeta.class, SuiteComunicacion.class})
 public class TodosLosTests {
     
 }
