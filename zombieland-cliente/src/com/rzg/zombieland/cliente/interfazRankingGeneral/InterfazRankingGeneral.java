1d2aef2ad0bedc800ecca7b26b4ebc2ff50ef7e6
Nicolas L <Nicolas@Jacob>
2015-06-05 17:59:36 -0300

    Interfaz ranking general

---
+++ zombieland-cliente/src/com/rzg/zombieland/cliente/interfazRankingGeneral/InterfazRankingGeneral.java
@@ -0,0 +1,129 @@
+package com.rzg.zombieland.cliente.interfazRankingGeneral;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class InterfazRankingGeneral extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InterfazRankingGeneral frame = new InterfazRankingGeneral();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public InterfazRankingGeneral() {
+		setTitle("Ranking General");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 600);
+		setResizable(false);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane
+				.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setBounds(0, 46, 797, 257);
+		contentPane.add(scrollPane);
+
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		scrollPane.setViewportView(tabbedPane);
+
+		JLabel lblNewLabel = new JLabel("Nombre de Usuario");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel.setBounds(40, 21, 205, 27);
+		contentPane.add(lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("Puntaje");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel_1.setBounds(668, 12, 95, 44);
+		contentPane.add(lblNewLabel_1);
+
+		JButton btnVolver = new JButton("Volver al Lobby");
+		btnVolver.setBounds(40, 325, 121, 35);
+		contentPane.add(btnVolver);
+
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2
+				.setIcon(new ImageIcon(
+						InterfazRankingGeneral.class
+								.getResource("/com/rzg/zombieland/cliente/interfazRankingGeneral/zombieBanner.png")));
+		lblNewLabel_2.setBounds(0, 304, 797, 247);
+		contentPane.add(lblNewLabel_2);
+
+		// MENU
+
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+
+		JMenu mnArchivo = new JMenu("Archivo");
+		menuBar.add(mnArchivo);
+
+		JMenuItem mntmIniciarSesion = new JMenuItem("Iniciar Sesion");
+		mnArchivo.add(mntmIniciarSesion);
+
+		JMenuItem mntmRegistrarse = new JMenuItem("Registrarse");
+		mnArchivo.add(mntmRegistrarse);
+
+		JMenuItem mntmSalir = new JMenuItem("Salir");
+		mnArchivo.add(mntmSalir);
+
+		JMenu mnPartida = new JMenu("Partida");
+		menuBar.add(mnPartida);
+
+		JMenuItem mntmJugar = new JMenuItem("Jugar");
+		mnPartida.add(mntmJugar);
+
+		JMenuItem mntmVerPartidas = new JMenuItem("Ver Partidas");
+		mnPartida.add(mntmVerPartidas);
+
+		JMenu mnCuenta = new JMenu("Cuenta");
+		menuBar.add(mnCuenta);
+
+		JMenuItem mntmDatos = new JMenuItem("Datos");
+		mnCuenta.add(mntmDatos);
+
+		JMenuItem mntmEstadsticas = new JMenuItem("Estad\u00EDsticas");
+		mnCuenta.add(mntmEstadsticas);
+
+		JMenu mnAyuda = new JMenu("Ayuda");
+		menuBar.add(mnAyuda);
+
+		JMenuItem mntmCmoJugar = new JMenuItem("C\u00F3mo Jugar");
+		mnAyuda.add(mntmCmoJugar);
+
+		JMenuItem mntmGoogleame = new JMenuItem("Googleame");
+		mnAyuda.add(mntmGoogleame);
+
+	}
+}
