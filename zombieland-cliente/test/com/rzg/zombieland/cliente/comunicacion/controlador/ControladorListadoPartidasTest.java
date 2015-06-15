575469365e118dc52a08b9421f9db6bbc98a8333
Nicolás Satragno <nsatragno@gmail.com>
2015-06-15 00:54:26 -0300

    Removido cross reference.

--- zombieland-cliente/test/com/rzg/zombieland/cliente/comunicacion/controlador/ControladorListadoPartidasTest.java
+++ zombieland-cliente/test/com/rzg/zombieland/cliente/comunicacion/controlador/ControladorListadoPartidasTest.java
@@ -1,6 +1,7 @@
 package com.rzg.zombieland.cliente.comunicacion.controlador;
 
-import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -9,18 +10,18 @@ import org.junit.Test;
 import com.google.gson.Gson;
 import com.rzg.zombieland.cliente.meta.Estado;
 import com.rzg.zombieland.cliente.meta.Estado.EscuchadorPartidas;
+import com.rzg.zombieland.comunes.comunicacion.pojo.POJOCreacionPartida;
+import com.rzg.zombieland.comunes.comunicacion.pojo.POJOPartida;
 import com.rzg.zombieland.comunes.comunicacion.respuesta.POJOListadoPartidas;
 import com.rzg.zombieland.comunes.comunicacion.respuesta.RespuestaGenerica;
 import com.rzg.zombieland.comunes.misc.ParametrosNoValidosException;
-import com.rzg.zombieland.server.controlador.AbstractPartidasTest;
-import com.rzg.zombieland.server.meta.ServicioPartidas;
 
 /**
  * Verifica el correcto funcionamiento del controlador de listado de partidas.
  * @author nicolas
  *
  */
-public class ControladorListadoPartidasTest extends AbstractPartidasTest {
+public class ControladorListadoPartidasTest {
 
     private static POJOListadoPartidas partidasRecibidas;
     
@@ -41,18 +42,16 @@ public class ControladorListadoPartidasTest extends AbstractPartidasTest {
      */
     @Test
     public void testSinPartidas() throws ParametrosNoValidosException {
-        assertEquals(0, pojoPartidasCreadas.size());
-        assertEquals(0, ServicioPartidas.getInstancia().getPartidas().size());
-        
+        List<POJOPartida> partidas = new ArrayList<POJOPartida>();
         Gson gson = new Gson();
         ControladorListadoPartidas controlador = new ControladorListadoPartidas();
         RespuestaGenerica respuesta = 
                 gson.fromJson(controlador.procesar(
-                                gson.toJson(new POJOListadoPartidas(pojoPartidasCreadas))),
+                                gson.toJson(new POJOListadoPartidas(partidas))),
                               RespuestaGenerica.class);
         Assert.assertTrue(respuesta.fuePeticionExitosa());
         Assert.assertNotNull(partidasRecibidas);
-        Assert.assertEquals(pojoPartidasCreadas, partidasRecibidas.getPartidas());
+        Assert.assertEquals(partidas, partidasRecibidas.getPartidas());
     }
     
     /**
@@ -61,26 +60,21 @@ public class ControladorListadoPartidasTest extends AbstractPartidasTest {
      */
     @Test
     public void testBasico() throws ParametrosNoValidosException {
+        List<POJOPartida> partidas = new ArrayList<POJOPartida>();
         final int CANTIDAD_PARTIDAS = 3;
-        crearPartidas(CANTIDAD_PARTIDAS);
+        for (int i = 0; i < CANTIDAD_PARTIDAS; i++) {
+            partidas.add(new POJOPartida(new POJOCreacionPartida(5, 5, "test"), "juan"));
+        }
         
         Gson gson = new Gson();
         ControladorListadoPartidas controlador = new ControladorListadoPartidas();
         RespuestaGenerica respuesta = 
                 gson.fromJson(controlador.procesar(
-                                gson.toJson(new POJOListadoPartidas(pojoPartidasCreadas))),
+                                gson.toJson(new POJOListadoPartidas(partidas))),
                               RespuestaGenerica.class);
         Assert.assertTrue(respuesta.fuePeticionExitosa());
         Assert.assertNotNull(partidasRecibidas);
-        Assert.assertEquals(pojoPartidasCreadas, partidasRecibidas.getPartidas());
-    }
-    
-    private void crearPartidas(final int CANTIDAD_PARTIDAS) throws ParametrosNoValidosException {
-        for (int i = 0; i < CANTIDAD_PARTIDAS; i++)
-            crearPartida();
-        // Verificamos que realmente se hayan creado tres partidas. Esto es un test... del test.
-        assertEquals(CANTIDAD_PARTIDAS, pojoPartidasCreadas.size());
-        assertEquals(CANTIDAD_PARTIDAS, ServicioPartidas.getInstancia().getPartidas().size());
+        Assert.assertEquals(partidas, partidasRecibidas.getPartidas());
     }
 }
 
